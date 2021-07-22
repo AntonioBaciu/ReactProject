cf163cebadd7b24c247fbb0b1d11df101fd25ccc
AntonioBaciu <antoniopaulobaciu@gmail.com>
2021-07-22 14:52:42 +0200

    Uploading logo

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,12 @@
+import Header from "./components/ui/Header";
 import "./App.css";
 
 const App = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
